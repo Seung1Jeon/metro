f4b8e290680167eeebfc9734eaef57295b088afa
achievebin <achieve7384@gmail.com>
2025-07-07 14:51:12 +0900

    .gitignore 생성

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from data_loader import load_total_graph
-from dijkstra import dijkstra
+from shortest_path import dijkstra
 
 graph = load_total_graph()
 
 start_station = '설화명곡'
 end_station = '안심'
+
+if start_station not in graph:
+    raise ValueError(f"출발역 '{start_station}' 이(가) 존재하지 않습니다.")
+
+if end_station not in graph:
+    raise ValueError(f"도착역 '{end_station}' 이(가) 존재하지 않습니다.")
 
 distance, path = dijkstra(graph, start_station, end_station)
 
